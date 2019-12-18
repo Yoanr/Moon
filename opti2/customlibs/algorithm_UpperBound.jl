dd47bc22b6628f97bd80e7e13be0644c36043f4b
Yoan <rockyoan@gmail.com>
2019-12-18 20:10:20 +0100

    upperbound moved

--- opti2/customlibs/algorithm_UpperBound.jl
+++ opti2/customlibs/algorithm_UpperBound.jl
@@ -2,6 +2,7 @@
 Author : ROCK - MORVAN
 Projet OPTIMISATION 2 - ENSIIE - 2019-2020
 """
+
 # Ne pas enlever
 include("./main.jl")
 
@@ -75,6 +76,11 @@ function alreadyPaidN(index,moonMap,moonPrices)
     return localMa
 end
 
+@doc """
+Cette fonction renvoi la borne supérieur du problème Moon Ad pour une instance donnée en argument
+argument : inst -> Instance de Moon Ad
+renvoi : Int64 -> Borne supérieur
+""" ->
 function getUpperBound(inst)
 
   moonPrices = copy(inst.ω)
@@ -117,33 +123,3 @@ function getUpperBound(inst)
   end
   return UpperBound
 end
-
-
-@doc """
-Modifie la solution sol de l'instance inst avec l'algorithme suivant
-*** DECRIRE VOTRE ALGORITHME ICI en quelques mots ***
-
-Cette fonction peut renvoyer n'importe quel type de variable (dont rien du tout), qui sera ensuite traité dans post_process. 
-""" ->
-function run(inst, sol)
-  upperBound = getUpperBound(inst)
-   print("upperBound : ")
-  print(upperBound)
-  print("\n")
-end
-
-@doc """
-Cette fonction est appelée après la fonction `run` et permet de faire de l'affichage et des traitement sur la sortie de la fonction `run` ; sans pour autant affecter son temps de calcul.
-
-Le paramètre cpu time est le temps de calcul de `run`. Les valeurs de `inst` et `sol` sont les mêmes qu’à la sortie de la fonction run. Enfin, `others` est ce qui est renvoyé par la fonction `run`. Vous pouvez ainsi effectuer des tests et afficher des résultats sans affecter le temps de calcul.
-""" ->
-function post_process(cpu_time::Float64, inst, sol, others)
-  # Remplir la fonction
-end
-
-# Ne pas enlever
-if length(ARGS) > 0
-  input_file = ARGS[1]
-  main(input_file)
-end
-
